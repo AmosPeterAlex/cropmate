fbef4b9f3498242c31fc4aeb343b716570f484fb
anuvindajayan <anuvindajayan@gmail.com>
2024-02-26 15:04:59 +0530

    add textfield refractor

---
+++ lib/global_widgets/textfield.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TextFieldScreen extends StatelessWidget {
+  final Color? textColour;
+  final Icon? suffixIcon;
+  final Color? iconColour;
+  final FontWeight? fontModal;
+  final String hintText;
+  final Icon? prefixIcon;
+  final int? maxLines;
+
+  TextFieldScreen(
+      {super.key,
+      this.prefixIcon,
+      this.textColour,
+      this.suffixIcon,
+      this.iconColour,
+      this.fontModal,
+      required this.hintText,
+      this.maxLines});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: TextFormField(
+          maxLines: maxLines,
+          decoration: InputDecoration(
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              hintText: hintText,
+              prefixIcon: prefixIcon,
+              suffixIcon: suffixIcon,
+              iconColor: iconColour)),
+    );
+  }
+}
