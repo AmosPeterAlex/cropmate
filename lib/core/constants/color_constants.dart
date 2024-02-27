e1a4007d9a027bc7afe938401b0a500b46129054
AmosPeterAlex <amospeteralex28@gmail.com>
2024-02-27 16:56:44 +0530

    color constants updated

---
+++ lib/core/constants/color_constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Color primaryColor = Color(0xFF208B3A); //green
+  static const Color bgColor = Color(0xFFFFC107); //yellow
+  static const Color whiteColor = Color(0xFFFFFFFF); //white
+  static const Color blackColor = Color(0xFF000000); //black
+  static const Color iconColor = Color(0XFF374957); //DOUBT
+  static const Color redColor = Color(0XFFB80000); //red
+}
