a79309dfa5cdf6c86b6a34d35f582fa8296cd808
AmosPeterAlex <amospeteralex28@gmail.com>
2024-03-01 10:36:45 +0530

    thursdays edits

--- lib/presentation/common/login_page_screen/view/login_page_screen.dart
+++ lib/presentation/common/login_page_screen/view/login_page_screen.dart
@@ -1,9 +1,10 @@
+
 import 'package:cropmate/core/constants/color_constants.dart';
 import 'package:cropmate/core/constants/image_constants.dart';
 import 'package:cropmate/global_widgets/matterial_button_widget.dart';
 import 'package:cropmate/global_widgets/textfield.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 
 class LoginPageScreen extends StatelessWidget {
   const LoginPageScreen({super.key});
@@ -14,36 +15,116 @@ class LoginPageScreen extends StatelessWidget {
     var devWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-          minimum: EdgeInsets.only(top: devHeight * 0.04),
-          child: ListView(children: [
-            Column(
+          minimum: EdgeInsets.only(top: devHeight * 0.01),
+          child: Padding(
+            padding: EdgeInsets.all(devHeight * .02),
+            child: Column(
+              //Listview deleted-thought it was not necessary
               children: [
-                Container(
-                  height: devHeight * .35,
-                  width: devWidth * .35,
-                  child: Image.asset(ImageConstants.appIcon),
-                ),
+                Stack(children: [
+                  SizedBox(
+                    height: devHeight * .2,
+                    width: devWidth * .2,
+                    child: Image.asset(ImageConstants.appIcon),
+                  ),
+                  Positioned(
+                    bottom: devHeight * .05,
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                            color: ColorConstants.blackColor, fontSize: 16),
+                        children: [
+                          TextSpan(
+                            text: "\tCrop",
+                            style: TextStyle(letterSpacing: .5),
+                          ),
+                          WidgetSpan(
+                            alignment: PlaceholderAlignment.middle,
+                            child: Text(
+                              'Mate',
+                              style: TextStyle(
+                                  fontStyle: FontStyle.italic,
+                                  color: ColorConstants.redColor,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  letterSpacing: .3),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ]),
                 ListTile(
                   leading: Text(
                     "Log in",
                     style: TextStyle(
                         color: ColorConstants.blackColor,
                         fontWeight: FontWeight.bold,
-                        fontSize: devHeight * .035),
+                        fontSize: devHeight * .032),
                   ),
                 ),
                 TextFieldScreen(hintText: "Name"),
                 TextFieldScreen(hintText: "Password"),
-                SizedBox(
-                  height: devHeight * 0.020,
+                ListTile(
+                  visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+                  trailing: InkWell(
+                    onTap: () {},
+                    child: Text('Forgot Password?',
+                        style: TextStyle(
+                            fontSize: 16, color: ColorConstants.blackColor)),
+                  ),
                 ),
                 MaterialButtonWidget(
                   buttonText: "Log in",
                   buttonColor: ColorConstants.primaryColor,
                 ),
+                SizedBox(
+                  height: devHeight * .02,
+                ),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                        color: ColorConstants.blackColor, fontSize: 16),
+                    children: [
+                      TextSpan(
+                        text: "Don't Have an Account?\t",
+                        style: TextStyle(letterSpacing: .5),
+                      ),
+                      WidgetSpan(
+                        alignment: PlaceholderAlignment.middle,
+                        child: Text(
+                          '\tCreate Account',
+                          style: TextStyle(
+                              color: ColorConstants.primaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: .3),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // SizedBox(
+                //   height: devHeight * .18,
+                // ),
+                //TODO anuvind materialbutton widget add akiy karinnj edit akam
+                Text(
+                  'Create Account as Farmer',
+                  style:
+                      TextStyle(color: ColorConstants.blackColor, fontSize: 16),
+                ),
+                SizedBox(
+                  height: devHeight * .002,
+                ),
+                Text(
+                  'Create Account as User',
+                  style:
+                      TextStyle(color: ColorConstants.blackColor, fontSize: 16),
+                ),
               ],
             ),
-          ])),
+          )),
     );
   }
 }
