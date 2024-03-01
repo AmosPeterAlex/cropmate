134e04427aae57a33ed00b30ec4be06f30fd3032
AmosPeterAlex <amospeteralex28@gmail.com>
2024-03-01 17:19:25 +0530

    user and farmer list added and somethings refractored

--- lib/presentation/admin/sample_main.dart
+++ lib/presentation/admin/sample_main.dart
@@ -1,5 +1,7 @@
 import 'package:cropmate/presentation/admin/home_screen/view/home_screen.dart';
+import 'package:cropmate/presentation/admin/user_management_screen/view/user_management_screen.dart';
 import 'package:flutter/material.dart';
+
 
 void main() {
   //push akan ayi chumma add akiya aa
