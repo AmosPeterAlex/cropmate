523618ff31314bcc5132700b511c7d6bf79e4233
Sajinduglas <sajinduglas500@gmail.com>
2024-03-05 12:37:06 +0530

    modified soil analysuis and profile listtile

--- lib/global_widgets/farmer_widgets/Profile_listtile.dart
+++ lib/global_widgets/farmer_widgets/Profile_listtile.dart
@@ -32,38 +32,3 @@ class ProfileListTile extends StatelessWidget {
     );
   }
 }
-
-import 'package:flutter/material.dart';
-
-import '../../core/constants/color_constants.dart';
-
-class ProfileListTile extends StatelessWidget {
-  const ProfileListTile({
-    super.key,
-    required this.text,
-    required this.icon,
-    required this.onTileClick,
-  });
-
-  final String text;
-  final VoidCallback onTileClick;
-  final Icon icon;
-
-  @override
-  Widget build(BuildContext context) {
-    var devHeight = MediaQuery.of(context).size.height;
-    var devWidth = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.all(3.0),
-      child: ListTile(
-        onTap: onTileClick,
-        leading: icon,
-        title: Text(text),
-        trailing: Icon(
-          Icons.arrow_forward_ios_outlined,
-          color: ColorConstants.primaryColor,
-        ),
-      ),
-    );
-  }
-}
--- lib/presentation/farmer/soil_analysis_Screen/view/soil_analysis_screen.dart
+++ lib/presentation/farmer/soil_analysis_Screen/view/soil_analysis_screen.dart
@@ -20,54 +20,6 @@ class SoilAnalysisScreen extends StatelessWidget {
         minimum: EdgeInsets.only(top: devHeight * 0.04),
         child: ListView(
           padding:
-              EdgeInsets.only(left: devWidth * 0.02, right: devWidth * 0.02),
-          children: [
-            TextFieldScreen(hintText: "Nitrogen content"),
-            TextFieldScreen(hintText: "Potassium content"),
-            TextFieldScreen(hintText: "Phosphorus content"),
-            TextFieldScreen(hintText: "Temperature"),
-            TextFieldScreen(hintText: "Humidity"),
-            TextFieldScreen(
-              hintText: "Ph",
-            ),
-            TextFieldScreen(hintText: "Rainfall"),
-            SizedBox(
-              height: devHeight * 0.03,
-            ),
-            MaterialButtonWidget(
-              onPressed: () {},
-              buttonText: "Check",
-              buttonColor: ColorConstants.primaryColor,
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-import 'package:flutter/material.dart';
-
-import '../../../../core/constants/color_constants.dart';
-import '../../../../global_widgets/matterial_button_widget.dart';
-import '../../../../global_widgets/textfield.dart';
-
-class SoilAnalysisScreen extends StatelessWidget {
-  const SoilAnalysisScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    var devHeight = MediaQuery.of(context).size.height;
-    var devWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Soil Analysis and Crop Recommendation"),
-        //centerTitle: true,
-      ),
-      body: SafeArea(
-        minimum: EdgeInsets.only(top: devHeight * 0.04),
-        child: ListView(
-          padding:
           EdgeInsets.only(left: devWidth * 0.02, right: devWidth * 0.02),
           children: [
             TextFieldScreen(hintText: "Nitrogen content"),
