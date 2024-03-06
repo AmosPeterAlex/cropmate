b3cdf2fa40182f817fcaa9675171d12aaf820b9d
Sajinduglas <sajinduglas500@gmail.com>
2024-03-06 16:47:20 +0530

    home page added,detail page added,plant prediction edited

--- lib/presentation/common/plant _disease_prediction_screen/view/plant_disease_prediction_screen.dart
+++ lib/presentation/common/plant _disease_prediction_screen/view/plant_disease_prediction_screen.dart
@@ -1,10 +1,258 @@
+// import 'package:cropmate/core/constants/color_constants.dart';
+// import 'package:flutter/material.dart';
+//
+//
+//
+// class PlantDiseasePredictionScreen extends StatelessWidget {
+//   const PlantDiseasePredictionScreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     var devHeight = MediaQuery.of(context).size.height;
+//     var devWidth = MediaQuery.of(context).size.width;
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Plant Disease Prediction"),
+//         centerTitle: true,
+//       ),
+//       body: SafeArea(
+//         minimum: EdgeInsets.only(top: devHeight * 0.02),
+//         child: ListView(
+//           children: [
+//             ListTile(
+//               title: Text(
+//                 "Add image",
+//                 style: TextStyle(
+//                     fontSize: 25,
+//                     fontWeight: FontWeight.normal,
+//                     color: ColorConstants.blackColor),
+//               ),
+//             ),
+//             SizedBox(height: devHeight*0.02,),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//               children: [
+//                 SizedBox(
+//                   width: devWidth * 0.35,
+//                   height: devHeight * 0.06,
+//                   child: ElevatedButton.icon(
+//                       style: ButtonStyle(
+//                           shape: MaterialStatePropertyAll(
+//                               RoundedRectangleBorder(
+//                                   borderRadius: BorderRadius.circular(10))),
+//                           backgroundColor:
+//                           MaterialStatePropertyAll(Color(0xFFd6ebdc))),
+//                       onPressed: () {},
+//                       icon:
+//                       Icon(Icons.camera_alt_outlined, color:  ColorConstants.blackColor),
+//                       label: Text(
+//                         'Camera',
+//                         style: TextStyle(color:  ColorConstants.blackColor),
+//                       )),
+//                 ),
+//                 SizedBox(
+//                   width: devWidth * 0.35,
+//                   height: devHeight * 0.06,
+//                   child: ElevatedButton.icon(
+//                       style: ButtonStyle(
+//                           shape: MaterialStatePropertyAll(
+//                               RoundedRectangleBorder(
+//                                   borderRadius: BorderRadius.circular(10))),
+//                           backgroundColor:
+//                           MaterialStatePropertyAll(Color(0xFFd6ebdc))),
+//                       // onPressed: () =>
+//                       //     upload("gallery"),
+//                       onPressed: () {},
+//                       icon: Icon(Icons.photo, color:  ColorConstants.blackColor),
+//                       label: Text('Gallery',
+//                           style: TextStyle(color:  ColorConstants.blackColor))),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+// import 'dart:io';
+//
+// import 'package:cropmate/core/constants/color_constants.dart';
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+//
+// class PlantDiseasePredictionScreen extends StatefulWidget {
+//   const PlantDiseasePredictionScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   _PlantDiseasePredictionScreenState createState() =>
+//       _PlantDiseasePredictionScreenState();
+// }
+//
+// class _PlantDiseasePredictionScreenState
+//     extends State<PlantDiseasePredictionScreen> {
+//   late ImagePicker _imagePicker;
+//   XFile? _selectedImage;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _imagePicker = ImagePicker();
+//   }
+//
+//   Future<void> _getImageFromCamera() async {
+//     final XFile? image =
+//     await _imagePicker.pickImage(source: ImageSource.camera);
+//     _setImage(image);
+//   }
+//
+//   Future<void> _getImageFromGallery() async {
+//     final XFile? image =
+//     await _imagePicker.pickImage(source: ImageSource.gallery);
+//     _setImage(image);
+//   }
+//
+//   void _setImage(XFile? image) {
+//     if (image != null) {
+//       setState(() {
+//         _selectedImage = image;
+//       });
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     var devHeight = MediaQuery.of(context).size.height;
+//     var devWidth = MediaQuery.of(context).size.width;
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Plant Disease Prediction"),
+//         centerTitle: true,
+//       ),
+//       body: SafeArea(
+//         minimum: EdgeInsets.only(top: devHeight * 0.02),
+//         child: ListView(
+//           children: [
+//             ListTile(
+//               title: Text(
+//                 "Add image",
+//                 style: TextStyle(
+//                     fontSize: 25,
+//                     fontWeight: FontWeight.normal,
+//                     color: ColorConstants.blackColor),
+//               ),
+//             ),
+//             SizedBox(height: devHeight * 0.02),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceAround,
+//               children: [
+//                 SizedBox(
+//                   width: devWidth * 0.35,
+//                   height: devHeight * 0.06,
+//                   child: ElevatedButton.icon(
+//                     style: ButtonStyle(
+//                       shape: MaterialStateProperty.all(RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.circular(10))),
+//                       backgroundColor:
+//                       MaterialStateProperty.all(Color(0xFFd6ebdc)),
+//                     ),
+//                     onPressed: _getImageFromCamera,
+//                     icon: Icon(Icons.camera_alt_outlined,
+//                         color: ColorConstants.blackColor),
+//                     label: Text(
+//                       'Camera',
+//                       style: TextStyle(color: ColorConstants.blackColor),
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(
+//                   width: devWidth * 0.35,
+//                   height: devHeight * 0.06,
+//                   child: ElevatedButton.icon(
+//                     style: ButtonStyle(
+//                       shape: MaterialStateProperty.all(RoundedRectangleBorder(
+//                           borderRadius: BorderRadius.circular(10))),
+//                       backgroundColor:
+//                       MaterialStateProperty.all(Color(0xFFd6ebdc)),
+//                     ),
+//                     onPressed: _getImageFromGallery,
+//                     icon: Icon(Icons.photo, color: ColorConstants.blackColor),
+//                     label: Text('Gallery',
+//                         style: TextStyle(color: ColorConstants.blackColor)),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             SizedBox(height: devHeight * 0.02),
+//             if (_selectedImage != null)
+//               Padding(
+//                 padding: EdgeInsets.all(devWidth * 0.02),
+//                 child: Image.file(
+//                   File(_selectedImage!.path),
+//                   height: devHeight * 0.3,
+//                   width: devWidth,
+//                   fit: BoxFit.cover,
+//                 ),
+//               ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+import 'dart:io';
+
 import 'package:cropmate/core/constants/color_constants.dart';
+import 'package:cropmate/global_widgets/matterial_button_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
+class PlantDiseasePredictionScreen extends StatefulWidget {
+  const PlantDiseasePredictionScreen({Key? key}) : super(key: key);
 
+  @override
+  _PlantDiseasePredictionScreenState createState() =>
+      _PlantDiseasePredictionScreenState();
+}
 
-class PlantDiseasePredictionScreen extends StatelessWidget {
-  const PlantDiseasePredictionScreen({super.key});
+class _PlantDiseasePredictionScreenState
+    extends State<PlantDiseasePredictionScreen> {
+  late ImagePicker _imagePicker;
+  XFile? _selectedImage;
+
+  @override
+  void initState() {
+    super.initState();
+    _imagePicker = ImagePicker();
+  }
+
+  Future<void> _getImageFromCamera() async {
+    final XFile? image =
+        await _imagePicker.pickImage(source: ImageSource.camera);
+    _setImage(image);
+  }
+
+  Future<void> _getImageFromGallery() async {
+    final XFile? image =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+    _setImage(image);
+  }
+
+  void _setImage(XFile? image) {
+    if (image != null) {
+      setState(() {
+        _selectedImage = image;
+      });
+    }
+  }
+
+  void _predictDisease() {
+    // Add your plant disease prediction logic here
+    // You can use the selected image (_selectedImage) for prediction
+    // For now, let's just print a message to the console
+    print('Performing plant disease prediction...');
+    Text("'Performing plant disease prediction...'");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +276,7 @@ class PlantDiseasePredictionScreen extends StatelessWidget {
                     color: ColorConstants.blackColor),
               ),
             ),
-            SizedBox(height: devHeight*0.02,),
+            SizedBox(height: devHeight * 0.02),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -36,38 +284,56 @@ class PlantDiseasePredictionScreen extends StatelessWidget {
                   width: devWidth * 0.35,
                   height: devHeight * 0.06,
                   child: ElevatedButton.icon(
-                      style: ButtonStyle(
-                          shape: MaterialStatePropertyAll(
-                              RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10))),
-                          backgroundColor:
-                          MaterialStatePropertyAll(Color(0xFFd6ebdc))),
-                      onPressed: () {},
-                      icon:
-                      Icon(Icons.camera_alt_outlined, color:  ColorConstants.blackColor),
-                      label: Text(
-                        'Camera',
-                        style: TextStyle(color:  ColorConstants.blackColor),
-                      )),
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xFFd6ebdc)),
+                    ),
+                    onPressed: _getImageFromCamera,
+                    icon: Icon(Icons.camera_alt_outlined,
+                        color: ColorConstants.blackColor),
+                    label: Text(
+                      'Camera',
+                      style: TextStyle(color: ColorConstants.blackColor),
+                    ),
+                  ),
                 ),
                 SizedBox(
                   width: devWidth * 0.35,
                   height: devHeight * 0.06,
                   child: ElevatedButton.icon(
-                      style: ButtonStyle(
-                          shape: MaterialStatePropertyAll(
-                              RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10))),
-                          backgroundColor:
-                          MaterialStatePropertyAll(Color(0xFFd6ebdc))),
-                      // onPressed: () =>
-                      //     upload("gallery"),
-                      onPressed: () {},
-                      icon: Icon(Icons.photo, color:  ColorConstants.blackColor),
-                      label: Text('Gallery',
-                          style: TextStyle(color:  ColorConstants.blackColor))),
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xFFd6ebdc)),
+                    ),
+                    onPressed: _getImageFromGallery,
+                    icon: Icon(Icons.photo, color: ColorConstants.blackColor),
+                    label: Text('Gallery',
+                        style: TextStyle(color: ColorConstants.blackColor)),
+                  ),
                 ),
               ],
+            ),
+            SizedBox(height: devHeight * 0.02),
+            if (_selectedImage != null)
+              Padding(
+                padding: EdgeInsets.all(devWidth * 0.02),
+                child: Image.file(
+                  File(_selectedImage!.path),
+                  height: devHeight * 0.3,
+                  width: devWidth,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            SizedBox(height: devHeight * 0.02),
+            MaterialButtonWidget(
+              onPressed: _predictDisease,
+              buttonText: 'Predict Disease',
+              buttonColor:
+                  ColorConstants.primaryColor, // Set your desired button color
             ),
           ],
         ),
--- lib/presentation/farmer/home_screen/view/home_screen.dart
+++ lib/presentation/farmer/home_screen/view/home_screen.dart
@@ -1,23 +1,57 @@
 import 'package:cropmate/core/constants/color_constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../global_widgets/crop_mate_icon_widget.dart';
+import '../../../../global_widgets/item_card.dart';
+import '../../../common/product_detail_screen/view/product_detail_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var devHeight = MediaQuery.of(context).size.height;
+    var devWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Home',
-          style: TextStyle(
-              color: ColorConstants.blackColor, fontWeight: FontWeight.bold),
+        // appBar: AppBar(
+        //   centerTitle: true,
+        //   title: Text(
+        //     'Home',
+        //     style: TextStyle(
+        //         color: ColorConstants.blackColor, fontWeight: FontWeight.bold),
+        //   ),
+        // ),
+        // body: Center(
+        //   child: Text("Home screen"),
+        // ),
+        appBar: AppBar(
+          title: CropMateIconWidget(),
+          centerTitle: true,
+          toolbarHeight: devHeight * .1,
+          actions: [
+            IconButton(
+                onPressed: () {}, icon: Icon(CupertinoIcons.cart_badge_plus))
+          ],
         ),
-      ),
-      body: Center(
-        child: Text("Home screen"),
-      ),
-    );
+        body: Padding(
+          padding: EdgeInsets.all(devHeight * 0.01),
+          child: GridView.builder(
+              itemCount: 12,
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: .58,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: devHeight * .008,
+                  mainAxisSpacing: devWidth * 0.02),
+              itemBuilder: (context, index) {
+                return InkWell(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ItemDetailsScreen())),
+                    child: ItemCard());
+              }),
+        ));
   }
 }
