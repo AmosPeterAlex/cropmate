62fef9ca78484745528cb2e7706b1638a3a79fb8
AmosPeterAlex <amospeteralex28@gmail.com>
2024-03-06 17:09:36 +0530

    qwert

--- lib/get_started_screen/get_started_screen.dart
+++ lib/get_started_screen/get_started_screen.dart
@@ -1,4 +1,7 @@
 import 'package:cropmate/presentation/admin/home_screen/view/home_screen.dart';
+import 'package:cropmate/presentation/farmer/bottom_navigation_screen/view/bottom_navigation_screen.dart';
+import 'package:cropmate/presentation/user/user_bottom_navigation_screen/controller/user_bottom_nav_screen_controller.dart';
+import 'package:cropmate/presentation/user/user_bottom_navigation_screen/view/user_bottom_nav_screen.dart';
 import 'package:flutter/material.dart';
 
 class GetStartedScreen extends StatelessWidget {
@@ -18,18 +21,28 @@ class GetStartedScreen extends StatelessWidget {
             children: [
               InkWell(
                 onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) =>HomeScreen() ,));
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>HomeScreen() ,),);
                 },
                 child: Container(height: 60,
                 width: double.infinity,
                 color: Colors.amber,),
               ),SizedBox(height: 20,),
-              Container(height: 60,
-              width: double.infinity,
-              color: Colors.amber,),SizedBox(height: 20,),
-              Container(height: 60,
-              width: double.infinity,
-              color: Colors.amber,),SizedBox(height: 20,),
+              InkWell(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>UserBottomNavScreen() ,),);
+                },
+                child: Container(height: 60,
+                width: double.infinity,
+                color: Colors.amber,),
+              ),SizedBox(height: 20,),
+              InkWell(
+               onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>BottomNavigationScreen() ,),);
+                },
+                child: Container(height: 60,
+                width: double.infinity,
+                color: Colors.amber,),
+              ),SizedBox(height: 20,),
             ],
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@
 
 import 'package:cropmate/get_started_screen/get_started_screen.dart';
 import 'package:cropmate/presentation/admin/home_screen/view/home_screen.dart';
+import 'package:cropmate/presentation/farmer/bottom_navigation_screen/controller/bottom_navigation_controller.dart';
+import 'package:cropmate/presentation/user/user_bottom_navigation_screen/controller/user_bottom_nav_screen_controller.dart';
+import 'package:cropmate/presentation/user/user_harvested_item_screen/controller/user_harvested_item_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,9 +18,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: GetStartedScreen(),
-      debugShowCheckedModeBanner: false,
+    return MultiProvider(
+      providers: [
+          ChangeNotifierProvider(
+            create: (context) => BottomNavigationController()),
+        ChangeNotifierProvider(create: (context) => UserBottomNavController(),),
+        ChangeNotifierProvider(create: (context) => UserHarvestedItemScreenController(),),
+        ],
+      child: MaterialApp(
+        home: GetStartedScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
