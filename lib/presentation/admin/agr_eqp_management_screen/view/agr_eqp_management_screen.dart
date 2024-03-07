deab2ab66ebfd904f4756e51695d9c17dd5b667e
AmosPeterAlex <amospeteralex28@gmail.com>
2024-03-07 15:22:52 +0530

    7/3

--- lib/presentation/admin/agr_eqp_management_screen/view/agr_eqp_management_screen.dart
+++ lib/presentation/admin/agr_eqp_management_screen/view/agr_eqp_management_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cropmate/global_widgets/image_icon_button.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/constants/color_constants.dart';
@@ -9,23 +10,51 @@ class AgrEqpManagementScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var devHeight = MediaQuery.of(context).size.height;
+    var devWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('Agr Equip Management'),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            TextFieldScreen(hintText: 'Equipment Name'),
-            // TextFieldScreen(hintText: 'Photo',maxLines: 4,),TODO photo de add akenm
-            TextFieldScreen(hintText: 'Price'),
-            TextFieldScreen(hintText: 'Quantity'),
-            TextFieldScreen(hintText: 'Brand'),
-            MaterialButtonWidget(
-                buttonText: 'Add Equipment',
-                onPressed: () {},
-                buttonColor: ColorConstants.primaryColor)
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFieldScreen(hintText: 'Equipment Name'),
+              TextFieldScreen(hintText: 'Price'),
+              TextFieldScreen(hintText: 'Quantity'),
+              TextFieldScreen(hintText: 'Brand'),
+              Align(
+                  child: Text('\tAdd Equipment Image'),
+                  alignment: Alignment.bottomLeft),
+              SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ImageIconButton(
+                      width: devWidth * .35,
+                      height: devHeight * .06,
+                      onPressed: () {},
+                      icon: Icons.camera_alt_outlined,
+                      label: 'Camera'),
+                  ImageIconButton(
+                      width: devWidth * .35,
+                      height: devHeight * .06,
+                      onPressed: () {},
+                      icon: Icons.photo,
+                      label: 'Gallery'),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              MaterialButtonWidget(
+                  buttonText: 'Add Equipment',
+                  onPressed: () {},
+                  buttonColor: ColorConstants.primaryColor)
+            ],
+          ),
         ),
       ),
     );
