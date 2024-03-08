61fd1bf449f8717139d0b86bf8de22ebb7db8d3a
anuvindajayan <anuvindajayan@gmail.com>
2024-03-08 12:56:38 +0530

    add cart function

--- lib/presentation/user/user_harvested_item_screen/view/user_harvested_item_screen.dart
+++ lib/presentation/user/user_harvested_item_screen/view/user_harvested_item_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cropmate/global_widgets/crop_mate_icon_widget.dart';
 import 'package:cropmate/global_widgets/item_card.dart';
+import 'package:cropmate/presentation/common/cart_screen/view/cart_screen.dart';
+import 'package:cropmate/presentation/common/product_detail_screen/view/product_detail_screen.dart';
 import 'package:cropmate/presentation/user/user_harvested_item_screen/controller/user_harvested_item_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,61 +14,81 @@ class UserHarvestedItemScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    UserHarvestedItemScreenController provider = Provider.of<UserHarvestedItemScreenController>(context);
+    UserHarvestedItemScreenController provider =
+        Provider.of<UserHarvestedItemScreenController>(context);
     var devHeight = MediaQuery.of(context).size.height;
     var devWidth = MediaQuery.of(context).size.width;
     return SafeArea(
-      child: DefaultTabController(
-        length: provider.categorylist.length,
-        initialIndex: 0,
-        child: Scaffold(
-            appBar: AppBar(centerTitle: true,automaticallyImplyLeading: false,
-                elevation: 0,
-                backgroundColor: ColorConstants.whiteColor,
-                titleTextStyle: const TextStyle(
+        child: DefaultTabController(
+      length: provider.categorylist.length,
+      initialIndex: 0,
+      child: Scaffold(
+          appBar: AppBar(
+              centerTitle: true,
+              automaticallyImplyLeading: false,
+              elevation: 0,
+              backgroundColor: ColorConstants.whiteColor,
+              titleTextStyle: const TextStyle(
+                  color: ColorConstants.primaryColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600),
+              toolbarHeight: devWidth * .25,
+              title: CropMateIconWidget(),
+              actions: [
+                Padding(
+                  padding: EdgeInsets.all(devHeight * 0.01),
+                  child: IconButton( onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>cartScreen()));
+                  }, icon: Icon(CupertinoIcons.cart,),),
+                )
+              ],
+              bottom: TabBar(
+                physics: BouncingScrollPhysics(),
+                tabAlignment: TabAlignment.fill,
+                labelStyle: const TextStyle(
                     color: ColorConstants.primaryColor,
-                    fontSize: 20,
                     fontWeight: FontWeight.w600),
-                toolbarHeight: devWidth * .25,
-                title: CropMateIconWidget(),
-                actions: [Padding(
-                  padding: EdgeInsets.all(devHeight*0.01),
-                  child: Icon(CupertinoIcons.cart),
-                )],
-                bottom: TabBar(physics: BouncingScrollPhysics(),tabAlignment: TabAlignment.fill,
-                  labelStyle: const TextStyle(
-                      color: ColorConstants.primaryColor, fontWeight: FontWeight.w600),
-                  labelColor: ColorConstants.blackColor,
-                  unselectedLabelColor: ColorConstants.primaryColor,
-                  // indicatorPadding: EdgeInsets.symmetric(horizontal: 5),
-                  unselectedLabelStyle: const TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.w500),
-                  indicatorSize: TabBarIndicatorSize.tab,
-                  indicator: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10)),
-                  tabs: List.generate(
-                    provider.categorylist.length,
-                        (index) => Tab(
-                      text: provider.categorylist[index],
-                    ),
+                labelColor: ColorConstants.blackColor,
+                unselectedLabelColor: ColorConstants.primaryColor,
+                // indicatorPadding: EdgeInsets.symmetric(horizontal: 5),
+                unselectedLabelStyle:
+                    const TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicator:
+                    BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                tabs: List.generate(
+                  provider.categorylist.length,
+                  (index) => Tab(
+                    text: provider.categorylist[index],
                   ),
-                  onTap: (value) {
-                    provider.onTap(index: value);
-                  },
-                )),
+                ),
+                onTap: (value) {
+                  provider.onTap(index: value);
+                },
+              )),
           body: Padding(
-            padding:  EdgeInsets.all(devHeight*0.01),
+            padding: EdgeInsets.all(devHeight * 0.01),
             child: GridView.builder(
-                itemCount: 12,
+                itemCount: itemList.length,
                 shrinkWrap: true,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     childAspectRatio: .65,
                     crossAxisCount: 2,
-                    crossAxisSpacing: devHeight *.008,
+                    crossAxisSpacing: devHeight * .008,
                     mainAxisSpacing: devWidth * 0.02),
                 itemBuilder: (context, index) {
-                  return ItemCard();
+                  return InkWell(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>ItemDetailsScreen()));
+                      },
+                      child: ItemCard(
+                        title: itemList[index].name,
+                        imageUrl: itemList[index].image,
+                        price: itemList[index].price,
+                        quantity: itemList[index].quantity,
+                        item: itemList[index],
+
+                      ));
                 }),
           )),
     ));
