9700c5ffd4991b88860a87b8875e9f2be84046f9
Sajinduglas <sajinduglas500@gmail.com>
2024-03-17 19:11:29 +0530

    ui change in govt scheme

--- lib/presentation/common/govt_scheme_screen/view/govt_scheme_screen.dart
+++ lib/presentation/common/govt_scheme_screen/view/govt_scheme_screen.dart
@@ -17,33 +17,33 @@ class GovtSchemeScreen extends StatelessWidget {
                 title: 'Some Random govt Scheme',
                 description:
                     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-                link: 'Fake link..........'),
+                link: 'Fake link..........',startAge: 'Start age  23', endAge: ' End age  26',),
             //should be wrapped with inkwell or something
             GovtSchemeCard(
                 title: 'Some Random govt Scheme',
                 description:
                     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-                link: 'Fake link..........'),
+                link: 'Fake link..........',startAge: 'Start age  23', endAge: ' End age  26',),
             GovtSchemeCard(
                 title: 'Some Random govt Scheme',
                 description:
                     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-                link: 'Fake link..........'),
+                link: 'Fake link..........',startAge: 'Start age  23', endAge: ' End age  26',),
             GovtSchemeCard(
                 title: 'Some Random govt Scheme',
                 description:
                     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-                link: 'Fake link..........'),
+                link: 'Fake link..........',startAge: 'Start age  23', endAge: ' End age  26',),
             GovtSchemeCard(
                 title: 'Some Random govt Scheme',
                 description:
                     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-                link: 'Fake link..........'),
+                link: 'Fake link..........',startAge: 'Start age  23', endAge: ' End age  26',),
             GovtSchemeCard(
                 title: 'Some Random govt Scheme',
                 description:
                     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-                link: 'Fake link..........'),
+                link: 'Fake link..........',startAge: 'Start age  23', endAge: ' End age  26',),
           ],
         ),
       ),
--- lib/repository/api/common/registration_screen/service/registartion_screen_service.dart
+++ lib/repository/api/common/registration_screen/service/registartion_screen_service.dart
@@ -8,7 +8,9 @@ class RegistrationService {
       var decodedData = await ApiHelper.postData(
           endPoint: "cropmapp/register/",
           body: data,
-          finalurl: 'http://10.11.0.236:8000/cropmapp/register/');
+           finalurl: 'http://10.0.2.2:8000/cropmapp/register/');
+          // finalurl: 'http://127.0.0.1:8000/cropmapp/register/');
+      // finalurl: 'http://10.11.0.236:8000/cropmapp/register/');
       var message = decodedData["message"];
       log(decodedData.toString());
       if (decodedData["status"] == 1) {
