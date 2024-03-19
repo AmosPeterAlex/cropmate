82799736360097f39c61e408b814128e079e51dc
anuvindajayan <anuvindajayan@gmail.com>
2024-03-19 10:32:32 +0530

    some changes

--- lib/presentation/common/govt_scheme_screen/controller/govt_scheme_controller.dart
+++ lib/presentation/common/govt_scheme_screen/controller/govt_scheme_controller.dart
@@ -1,9 +1,9 @@
 import 'dart:developer';
 
-import 'package:cropmate/repository/api/common/govt_scheme_screen/service/govt_scheme_service';
 import 'package:flutter/material.dart';
 
 import '../../../../repository/api/common/govt_scheme_screen/model/govt_scheme_model.dart';
+import '../../../../repository/api/common/govt_scheme_screen/service/govt_scheme_service.dart';
 
 class GovtSchemeController with ChangeNotifier {
   late GovtSchemeModel govtSchemeModel;
@@ -17,7 +17,6 @@ class GovtSchemeController with ChangeNotifier {
       var data = await GovtSchemeService.getGovtSchemes();
       if (data != null) {
         govtSchemeModel = GovtSchemeModel.fromJson(data);
-        
       } else {
         log('Failed to fetch government schemes');
       }
