3382ba65a2f3355b482a5d6d06fea24519c09a91
Sajinduglas <sajinduglas500@gmail.com>
2024-03-22 17:18:58 +0530

    govt scheme card edited,addharvesteditem controller edited,and screen also edited

--- lib/presentation/farmer/add_harvested_item_screen/controller/add_harvested_item_controller.dart
+++ lib/presentation/farmer/add_harvested_item_screen/controller/add_harvested_item_controller.dart
@@ -1,24 +1,130 @@
+import 'dart:convert';
 import 'dart:io';
 
-import 'package:cropmate/presentation/common/login_screen/view/login_screen.dart';
-import 'package:cropmate/presentation/farmer/home_screen/view/home_screen.dart';
-import 'package:cropmate/repository/api/common/registration_screen/service/registartion_screen_service.dart';
+import 'package:cropmate/repository/api/farmer/add_harvested_item/service/add_harvested_item_service.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-import '../../../../repository/api/farmer/add_harvested_item/service/add_harvested_item_service.dart';
+import '../../../../app_config/app_config.dart';
 
 class AddHarvestedItemController extends ChangeNotifier {
-  void onAdd({
-    required String createdBy,
-    required String cropType,
-    required String cropName,
-    required double price,
-    required int quantity,
-    required String description, required String isAvailable,
-    // File? imageToAdd,
-    // required String isAvailable,
-    // required BuildContext context,
-  }) {
+  void onAdd(
+      {required String createdBy,
+      required String cropType,
+      required String cropName,
+      required double price,
+      required int quantity,
+      String? description,
+      String? isAvailable,
+      required File? image}) async {
+    //Multipart image to be added
+    try {
+      // Upload image
+      var imageUrl = "${AppConfig.baseUrl}cropmapp/farmerproducts/";
+      var response = await onUpload(
+          imageUrl,
+          image,
+          createdBy,
+          cropType,
+          cropName,
+          price,
+          quantity,
+          description,
+          isAvailable);
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        var imageUrlFromServer = json.decode(response.body)["image"];
+        var data = {
+          "crop_type": cropType,
+          "crop_name": cropName,
+          "price": price.toString(),
+          "quantity": quantity.toString(),
+          "description": description,
+          "posted_by": createdBy,
+          "is_available": isAvailable,
+          "image": imageUrlFromServer
+        };
+
+        // Call API to add equipment
+        var decodedData = await AddHarvestedItemService.postHarvestedData(data);
+
+        if (decodedData["status"] == 1) {
+          // Equipment added successfully
+          print("Equipment Added Successfully");
+          print(" iiiifffff${response.body}");
+          // return decodedData;
+        } else {
+          // Handle error in API response
+          var message = "Error in API";
+          print(message);
+          print("elseeeeeee${response.body}");
+          // return decodedData;
+        }
+      } else {
+        // Image upload failed
+        // Handle error
+        print("Image upload failed with status code: ${response.statusCode}");
+      }
+    } catch (e) {
+      // Handle any exceptions
+      print("Error occurred: $e");
+    }
+  }
+
+  Future<http.Response> onUpload(
+      String url,
+      File? selectedImage,
+      String createdBy,
+      String cropType,
+      String cropName,
+      double price,
+      int quantity,
+      String? description,
+      String? isAvailable) async {
+    var request = http.MultipartRequest('POST', Uri.parse(url));
+    Map<String, String> headers = {"Content-type": "multipart/form-data"};
+
+    if (selectedImage != null) {
+      print("Image file size: ${selectedImage.lengthSync()} bytes <<<<<<<<<<<");
+      // var request = http.MultipartRequest('POST', Uri.parse(url));
+
+      // Add image file to the request
+      request.files.add(
+        await http.MultipartFile.fromPath(
+          'image',
+          selectedImage.path,
+        ),
+      );
+    }
+
+    if (createdBy != null) {
+      request.fields['posted_by'] = createdBy;
+    }
+    if (cropType != null) {
+      request.fields['crop_type'] = cropType;
+    }
+    if (cropName != null) {
+      request.fields['crop_name'] = cropName;
+    }
+    if (price != null) {
+      request.fields['price'] = price.toString();
+    }
+    if (quantity != null) {
+      request.fields['quantity'] = quantity.toString();
+    }
+    if (description != null) {
+      request.fields['description'] = description;
+    }
+    request.fields['is_available'] = isAvailable!;
+
+    request.headers.addAll(headers);
+    print("request: " + request.toString());
+    var res = await request.send();
+    return await http.Response.fromStream(res);
+  }
+}
+/*
+    sajins working code with error only in image uploadding
     var data = {
       "crop_type": cropType,
       "crop_name": cropName,
@@ -26,35 +132,20 @@ class AddHarvestedItemController extends ChangeNotifier {
       "quantity": quantity.toString(),
       "description": description,
       "posted_by": createdBy,
-      "is_available":isAvailable
-      // "image":imageToAdd
+      "is_available": isAvailable,
+      // "image": imgurl
     };
-    /*
-    var data =
-    {
-      "crop_type": "Vegetables",
-      "crop_name": "new veg",
-      "price": 40,
-      "quantity": 15,
-      "description": "tomatoos...sweet",
-      "posted_by": "gigi"
 
-    }
-    ;
-    */
     AddHarvestedItemService.postHarvestedData(data).then((decodedData) {
       // print("success i.e status ==1");
       if (decodedData["status"] == 1) {
         print("adding  success\n i.e status ==1");
-        // Navigator.pushAndRemoveUntil(
-        //     context,
-        //     MaterialPageRoute(builder: (context) => HomeScreen()),
-        //         (route) => false);
       } else {
         var message = "error in api";
         print(message);
-        // AppUtils.oneTimeSnackBar(message, context: context);
       }
     });
-  }
+      }
 }
+
+    */
